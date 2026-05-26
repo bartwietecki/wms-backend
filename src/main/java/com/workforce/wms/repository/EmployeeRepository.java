7548dcf8ed494a862bd4ee306b31255d962051dc
bartwietecki <bartekwieteckiyt@gmail.com>
2026-05-26 07:48:12 +0200

    resolve current employee from authenticated principal

--- src/main/java/com/workforce/wms/repository/EmployeeRepository.java
+++ src/main/java/com/workforce/wms/repository/EmployeeRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
 
     Page<Employee> findAllByActive(boolean active, Pageable pageable);
+
+    Optional<Employee> findByUser_Username(String username);
 }
